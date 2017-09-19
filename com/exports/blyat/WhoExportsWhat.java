e2711221f14fb2784c30f0631e18d4d7ecd04dc1
felluffy <somerandomghai@gmail.com>
2017-09-19 09:39:22 +0600

    random test

---
+++ com/exports/blyat/WhoExportsWhat.java
@@ -0,0 +1,59 @@
+package com.exports.blyat;
+import org.apache.commons.csv.*;
+import edu.duke.*;
+
+public class WhoExportsWhat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//.System.out.println("BLYAT");
+		WhoExportsWhat blek = new WhoExportsWhat(); 
+		blek.testWhoExports();
+	}
+	
+	void whoExports(String item, CSVParser parser)
+    {
+        for(CSVRecord record : parser)
+        {
+            if(record.get("Exports").contains(item))
+                System.out.println(record.get("Country"));
+        }
+    }
+    
+    String countryInfo(String country, CSVParser parser)
+    {
+        for(CSVRecord record : parser)
+        {
+            //System.out.println(record.get("Country"));
+            if(record.get("Country").contains(country))
+                //System.out.println(y);
+                return new String(record.get("Country") + ": " + record.get("Exports") + ": " + record.get("Value (dollars)"));
+        }
+        return "";
+    }
+    
+    void listExporterswoProducts(CSVParser parser, String exportItem1, String exportItem2)
+    {
+    	for(CSVRecord record : parser)
+        {
+            //System.out.println(record.get("Country"));
+            if(record.get("Exports").contains(exportItem1) && record.get("Exports").contains(exportItem2))
+                System.out.println(record.get("Country"));
+                //return new String(record.get("Country") + ": " + record.get("Exports") + ": " + record.get("Value (dollars)"));
+        }
+    }
+    
+    void testWhoExports()
+    {
+        FileResource fr = new FileResource();
+        
+        CSVParser parser = fr.getCSVParser();
+        //whoExports("coffee", parser);
+        System.out.println(countryInfo("Germany", parser));
+        
+        parser = fr.getCSVParser();
+        listExporterswoProducts(parser, "gold", "diamonds");
+        
+    }
+
+}
